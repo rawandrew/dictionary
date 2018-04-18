6492dba6875d7fd46e6d67636d7038a0e3cf6447
Andrei Crudu <andrei.crudu@yahoo.com>
2018-04-18 16:49:12 +0300

    Make it possible to extract a random word from the words text file dictionary.

--- lib/dictionary.ex
+++ lib/dictionary.ex
@@ -1,5 +1,10 @@
 defmodule Dictionary do
-  def hello do
-    IO.puts "Hello World!!!"
+
+  def random_word() do
+    Enum.random(word_list())
+  end
+  def word_list do
+    contents = File.read!("assets/words.txt")
+    list = String.split(contents, ~r/\n/)
   end
 end
