b7fb6efd85603dab0260d48b39d24d00efff167e
Andrei Crudu <andrei.crudu@yahoo.com>
2018-04-18 16:53:05 +0300

    Refactor the code to a functional approach.

--- lib/dictionary.ex
+++ lib/dictionary.ex
@@ -1,10 +1,12 @@
 defmodule Dictionary do
 
   def random_word() do
-    Enum.random(word_list())
+    word_list()
+    |> Enum.random()
   end
   def word_list do
-    contents = File.read!("assets/words.txt")
-    list = String.split(contents, ~r/\n/)
+    "assets/words.txt"
+    |> File.read!()
+    |> String.split(~r/\n/)
   end
 end
