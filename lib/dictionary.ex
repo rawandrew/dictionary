6269490c05126995d4e54a528c68d06bf821472a
Andrei Crudu <andrei.crudu@yahoo.com>
2018-04-19 14:01:11 +0300

    Improve the path finding to the words text file.

--- lib/dictionary.ex
+++ lib/dictionary.ex
@@ -5,7 +5,8 @@ defmodule Dictionary do
     |> Enum.random()
   end
   def word_list do
-    "assets/words.txt"
+    "../assets/words.txt"
+    |> Path.expand(__DIR__)
     |> File.read!()
     |> String.split(~r/\n/)
   end
