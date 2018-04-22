3ec98add52f74dcbcbd3a3cf51172e9c8122af8c
Andrei Crudu <andrei.crudu@yahoo.com>
2018-04-22 09:56:10 +0300

    Refactor the code so as to have a better split between the public API and the business logic.

--- lib/dictionary.ex
+++ lib/dictionary.ex
@@ -1,13 +1,6 @@
 defmodule Dictionary do
 
-  def random_word() do
-    word_list()
-    |> Enum.random()
-  end
-  def word_list do
-    "../assets/words.txt"
-    |> Path.expand(__DIR__)
-    |> File.read!()
-    |> String.split(~r/\n/)
-  end
+  defdelegate start(), to: Dictionary.WordList, as: :word_list
+  defdelegate random_word(), to: Dictionary.WordList
+
 end
---
+++ lib/dictionary/word_list.ex
@@ -0,0 +1,15 @@
+defmodule Dictionary.WordList do
+
+  def random_word() do
+    word_list()
+    |> Enum.random()
+  end
+
+  def word_list do
+    "../../assets/words.txt"
+    |> Path.expand(__DIR__)
+    |> File.read!()
+    |> String.split(~r/\n/)
+  end
+
+end
