9ee0fbd9d0454dd71994f545da550a7e4ea9994f
Andrei Crudu <andrei.crudu@yahoo.com>
2018-06-19 13:45:35 +0300

    Cache the word list in an agent

--- lib/dictionary/word_list.ex
+++ lib/dictionary/word_list.ex
@@ -1,5 +1,9 @@
 defmodule Dictionary.WordList do
 
+  def start_link() do
+    Agent.start_link(&word_list/0)
+  end
+
   def word_list() do
     "../../assets/words.txt"
     |> Path.expand(__DIR__)
@@ -7,8 +11,7 @@ defmodule Dictionary.WordList do
     |> String.split(~r/\n/)
   end
 
-  def random_word(word_list) do
-    word_list
-    |> Enum.random()
+  def random_word(agent) do
+    Agent.get(agent, &Enum.random/1)
   end
 end
